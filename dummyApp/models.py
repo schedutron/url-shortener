4125c153b584407aec019bbd2a9bc920e244993f
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-11-06 21:57:08 +0530

    Finalize

--- dummyApp/models.py
+++ dummyApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import string, random
 # Create your models here.
 class KirrURL(models.Model):
     url = models.CharField(max_length=220,)
@@ -11,7 +11,7 @@ class KirrURL(models.Model):
         print("something")
         if self.shortcode is None or self.shortcode == "":
             self.shortcode = code_generator()
-        #super(KirrURL, self).save()
+        super(KirrURL, self).save(*args, **kwargs)
     
     def __str__(self):
         return str(self.url)
