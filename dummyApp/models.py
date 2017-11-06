2fc3b188dc8a1e7a5dc7036213e142592726081a
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-11-06 21:22:33 +0530

    Add code_generator() and create_shortcode() functions

--- dummyApp/models.py
+++ dummyApp/models.py
@@ -15,4 +15,19 @@ class KirrURL(models.Model):
     
     def __str__(self):
         return str(self.url)
-    
+
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    new_code = ''
+    for _ in range(size):
+        new_code += random.choice(chars)
+    return new_code
+
+
+def create_shortcode(instance, size=6):
+    new_code = code_generator(size=size)
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(shortcode=new_code).exists()
+    if qs_exists:
+        return create_shortcode(size=size)
+    return new_code
